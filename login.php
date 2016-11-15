14345ee21a54fe7cc8aa3426e9bc676120be0c8e
Darko <dhranic2@foi.hr>
2016-11-15 19:16:54 +0100

    promjena odgovora
    na eng

--- login.php
+++ login.php
@@ -1,40 +1,40 @@
 <?php
 require_once ('dbclass.php');
 require_once ('funkcije.php');
-if(isset($_POST['username'])&&isset($_POST['password'])) {
+if(isset($_POST['username'])&&isset($_POST['password'])) { //dal su poslani username i pass
     $username = htmlentities($_POST['username']);
     $password = htmlentities($_POST['password']);
     $encPass = md5($password);
     $response = array();
-    if (empty($username) || empty($password)) {
+    if (empty($username) || empty($password)) {     //dal su upisani neki podaci
         $response["status"] = "error";
         $response["description"] = "Nisu upisani potrebni podaci!";
         echo json_encode($response);
         return;
     }
-    if (authorizeUser($username, $encPass) == true) {
+    if (authorizeUser($username, $encPass) == true) {   //provjera kombinacije usernamea i passworda
         $response["status"] = "success";
-        $response["description"] = "Uspješna prijava";
+        $response["description"] = "Logged in!";
         echo json_encode($response);
         return;
     }
     else {
-        if (userNameExistance($username) == 1||emailExistance($username)==1) {
+        if (userNameExistance($username) == 1||emailExistance($username)==1) {  //provjera postojanja korisnika
             $response["status"] = "error";
-            $response["description"] = "Pogrešna lozinka!";
+            $response["description"] = "Password is incorrect!";
             echo json_encode($response);
             return;
         }
         else {
             $response["status"] = "error";
-            $response["description"] = "Ne postoji korisnik!";
+            $response["description"] = "Username doesn't exist!";
             echo json_encode($response);
             return;
         }
     }
 }else{
     $response["status"] = "error";
-    $response["description"] = "Nisu upisani potrebni podaci!";
+    $response["description"] = "Incorrect or no data provided!";
     echo json_encode($response);
     return;
 }
--- register.php
+++ register.php
@@ -7,36 +7,36 @@ if(isset($_POST['username'])&&isset($_POST['password'])&&isset($_POST['email']))
     $email = htmlentities($_POST['email']);
     $encPass = md5($password);
     $response = array();
-    if (empty($username) || empty($password) || empty($email)||!(preg_match("/[A-Za-z0-9._%+-]{2,}@[A-Za-z0-9.-]{2,}\.[A-Za-z]{2,}/", $email))) {
+    if (empty($username) || empty($password) || empty($email)||!(preg_match("/[A-Za-z0-9._%+-]{2,}@[A-Za-z0-9.-]{2,}\.[A-Za-z]{2,}/", $email))) {   //provjera formata emaila
         $response["status"] = "error";
-        $response["description"] = "Nisu upisani pravilni podaci!";
+        $response["description"] = "Email format is not valid or empty field!";
         echo json_encode($response);
         return;
     }
-    $userNameExist = userNameExistance($username);
+    $userNameExist = userNameExistance($username);  //provjera zauzetosti username ili emaila
     $emailExist = emailExistance($email);
     if ($userNameExist==1||$emailExist==1) {
         $response["status"] = "error";
         if($userNameExist){
-            $response["description"] = "Korisnicko ime je već zauzeto!";
+            $response["description"] = "Username is already taken!";
         }
         else{
-            $response["description"] = "Email je već zauzet!";
+            $response["description"] = "Email is already taken!";
         }
         echo json_encode($response);
         return;
     }
     else {
-        $r = registerUser($username, $email, $encPass);
+        $r = registerUser($username, $email, $encPass); //registriranje korisnika
         if ($r) {
             $response["status"] = "success";
-            $response["description"] = "Korisnik registriran!";
+            $response["description"] = "User registered successfully!";
             echo json_encode($response);
             return;
         }
-        else {
+        else {  //nije uspjelo upisivanje u bazu
             $response["status"] = "error";
-            $response["description"] = "Doslo je do greske prilikom registracije!";
+            $response["description"] = "There was a problem during registration process! Try again, please!";
             echo json_encode($response);
             return;
         }
@@ -44,7 +44,7 @@ if(isset($_POST['username'])&&isset($_POST['password'])&&isset($_POST['email']))
 }
 else{
     $response["status"] = "error";
-    $response["description"] = "Nisu upisani potrebni podaci!";
+    $response["description"] = "Incorrect or no data provided!";
     echo json_encode($response);
     return;
 }
